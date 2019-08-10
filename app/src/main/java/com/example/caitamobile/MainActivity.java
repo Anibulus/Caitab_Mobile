ed081f5d379da819eec95e5285bdae0ce776ea5b
Luis Antonio Preza <anibulusnn@gmail.com>
2019-08-10 01:26:36 -0500

    Comentarios en el inicio de Sesion para facil comprencion y primera prueba de la Funcion ValidarCuenta

--- app/src/main/java/com/example/caitamobile/MainActivity.java
+++ app/src/main/java/com/example/caitamobile/MainActivity.java
@@ -13,31 +13,48 @@ import com.example.caitamobile.Constantes.ListaActividades;
 import com.example.caitamobile.Constantes.IntentExtras;
 import com.example.caitamobile.modelo.Usuario;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class MainActivity extends AppCompatActivity {
         private EditText etUsuario;
         private EditText etContrasenia;
         private Button btnIngresar;
         private Button btnCerrar;
+        private Conexion conexionMySQL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /**
+         * Aqui se enlazan los componentes que tiene el Activity (Inicio de sesion)
+         */
         etUsuario = findViewById(R.id.etUsuario);
         etContrasenia = findViewById(R.id.etContrasenia);
         btnIngresar = findViewById(R.id.btnIngresar);
         btnCerrar = findViewById(R.id.btnCerrar);
+        conexionMySQL=new Conexion();
 
+        /**
+         * Al pulsar el Boton Ingresar, se llama una funcion que validara el inicio de Sesion, similar a un DAO (Data access object)
+         */
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Usuario usuario = validarCuenta(etUsuario.getText().toString(), etContrasenia.getText().toString());
+                /**
+                 * Despues de realizar la funcion y conseguir un resultado, valida lo que la variable 'usuario' contiene
+                 */
 
                 if(usuario == null) {
-
+                    /**
+                     * En el caso de no haber encontrado un usuario, se notifica la persona que lo intente otra vez y borrara lo que contenga la contrasena
+                     */
+                    //etContrasena.setText("");
                     String texto = "Usuario o contraseña incorrectos";
                     Toast mensaje = Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_SHORT);
                     mensaje.show();
@@ -46,35 +63,64 @@ public class MainActivity extends AppCompatActivity {
 
                     mostrarSiguienteVista(usuario);
 
-                }
-
-            }
+                }//Fin de la validacion del usuario\
+            }//Fin del metodo onClick
 
             private Usuario validarCuenta(String usuario, String contrasenia) {
-                try {
+                /**
+                 * Se crea un objeto usuario nulo al principio y se llenara en
+                 * el caso de que el empleado se encuentre para coincidir
+                 * donde se llama la funcion
+                 */
+                Usuario usu=null;
+                Connection conn = conexionMySQL.CONN();
+                if (conn!=null) {
+                    String query = "select * from Usuario where Usuario=? and Contrasenia=?";
 
-                    // TODO - USUARIO = ... CONSULTA SQL
-                    return new Usuario(1, usuario, contrasenia); // ACTUALMENTE RETORNANDO OBJETO DUMMY
+                    PreparedStatement ps = null;
+                    try {
+                        ps = conn.prepareStatement(query);
+                        ps.setString(1, usuario);
+                        ps.setString(2, contrasenia);
+                        ResultSet rs = ps.executeQuery();
+                        if (rs.next()) {
+                            /**
+                             * Se llena el objeto USU cuando se encuentra lo que hace que permita llevarlo a la siguiente ventana
+                             */
+                            usu=new Usuario(rs.getInt("ID_Usu"), rs.getString("Usuario"), rs.getString("Contrasenia"));//id,usuario,contrasena
+                        } else {
+                            //TODO - Verificar si es posible poner un Toast mencionando posibles errores
+                        }
+                    } catch (java.sql.SQLException e) {
+                        //TODO - Verificar si es posible poner un Toast mencionando posibles errores
+                    }
 
-                } catch (SQLException ex) {
-                    return null;
-                }
-            }
+                } else {
+                    //TODO - Verificar si es posible poner un Toast mencionando posibles errores
+                }//Fin de SI la conexion a la base de datos es diferente de nula
+                /**
+                 * Retorna al usuario
+                 */
+                    return usu;//Retorna un objeto funcional
+            }//Fin de la funcion que valida el inicio de sesion
 
             private void mostrarSiguienteVista(Usuario usuario) {
 
-                /* INDICA DESDE QUE VISTA SE ESTA MANDANDO A LLAMAR EL MENU
+                /**
+                 * INDICA DESDE QUE VISTA SE ESTA MANDANDO A LLAMAR EL MENU
                  * HACIENDO USO DE ENUMS PARA EVITAR ERRORES POR TYPOS
                  * SE PUEDEN AGREGAR MAS 'EXTRAS' CONFORME SEA NECESARIO
-                 *
                  */
                 Intent intent = new Intent(MainActivity.this, Menu.class);
                 intent.putExtra(IntentExtras.DESDE.llave, ListaActividades.MAIN_ACTIVITY.nombre);
                 intent.putExtra(IntentExtras.USUARIO.llave, usuario);
                 startActivity(intent);
             }
-        });
+        });//Fin del metodo que escucha el boton de ingresar
 
+        /**
+         * Este metodo cierra la aplicacion
+         */
         btnCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
